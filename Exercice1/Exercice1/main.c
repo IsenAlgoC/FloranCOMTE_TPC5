4dfd28f235343fdea639131b92c3ef9436926610
Floran COMTE <floran.comte@isen.yncrea.fr>
2020-11-29 21:09:02 +0100

    push2

--- Exercice1/Exercice1/main.c
+++ Exercice1/Exercice1/main.c
@@ -13,6 +13,8 @@ int main() {
 	afficheTab(myTab1, 10, 3);
 
 	int* myTab2 = NULL;
+	int tabSize = TAB2SIZE;
+	int nbElts = 40;
 	myTab2 = (int*)malloc(TAB2SIZE * sizeof(int));
 	if (myTab2 != NULL) { 
 		initTab(myTab2, TAB2SIZE); 
@@ -21,10 +23,15 @@ int main() {
 		printf("Memoire insuffisante."); return (-1); 
 	}
 
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < nbElts; i++) {
 		myTab2[i] = i + 1;
 	}
 
 	afficheTab(myTab2, TAB2SIZE, 20);
+	ajoutElementDansTableau(myTab2, &tabSize, &nbElts, 24);
+	afficheTab(myTab2, TAB2SIZE, nbElts);
+
+	return (EXIT_SUCCESS);
+
 	free(myTab2);
 }
